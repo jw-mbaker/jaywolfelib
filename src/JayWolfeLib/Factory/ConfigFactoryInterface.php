750de378e890b344eecc4d13a9de38befa3a2092
Matthew <mbaker@jaywolfe.com>
2022-11-09 21:24:06 -0600

    Added documentation

--- src/JayWolfeLib/Factory/ConfigFactoryInterface.php
+++ src/JayWolfeLib/Factory/ConfigFactoryInterface.php
@@ -6,5 +6,12 @@ use JayWolfeLib\Config\ConfigInterface;
 
 interface ConfigFactoryInterface extends BaseFactoryInterface
 {
+	/**
+	 * Get a config instance from the container.
+	 * Instantiates it if it does not exist.
+	 *
+	 * @param string $plugin_file
+	 * @return ConfigInterface
+	 */
 	public function get(string $plugin_file): ConfigInterface;
 }
