307fa846298fc52c9c3e051f0d26b5d61a7c1e62
Matthew Baker <93605916+jw-mbaker@users.noreply.github.com>
2022-12-28 18:01:03 -0600

    Update CollectionInterface.php

--- src/JayWolfeLib/Collection/CollectionInterface.php
+++ src/JayWolfeLib/Collection/CollectionInterface.php
@@ -6,5 +6,5 @@ interface CollectionInterface extends \IteratorAggregate, \Countable
 {
 	public function all(): array;
 	public function get(string $name);
-	public function remove(string $name);
+	public function remove($name);
 }
